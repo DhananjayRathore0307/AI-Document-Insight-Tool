8503fe001c73919c8368fb0bad6d3fe546645fbc
DhananjayRathore <dhananjayrathore0307@gmail.com>
2025-06-28 18:58:06 +0530

    connection

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ai-document-insight-tool.vercel.app/", "http://localhost:5173"],
+    allow_origins=["https://ai-document-insight-tool-jgsq.vercel.app/", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
